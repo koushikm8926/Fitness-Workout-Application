d52e726f9de4ad7d28313ecbcf552fc23fa52613
Koushik Mondal <darkdeavil004@gmail.com>
2023-06-17 19:24:47 +0530

    Updated Home Screen

--- components/FitnessCards.js
+++ components/FitnessCards.js
@@ -1,9 +1,11 @@
 import { Pressable, StyleSheet, Text, View, Image } from "react-native";
 import React from "react";
 import fitness from "../data/fitness";
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+
 const FitnessCards = () => {
   const FitnessData = fitness;
-
   return (
     <View>
       {FitnessData.map((item, key) => (
@@ -16,6 +18,7 @@ const FitnessCards = () => {
             source={{ uri: item.image }}
           />
           <Text style={{position:'absolute',color:'white', fontSize:16, fontWeight:'bold', left:20,top:20,   }}>{item.name}</Text>
+          <MaterialCommunityIcons name="lightning-bolt" size={24} color="white" style={{position:'absolute', bottom:15,left:20,}} />
         </Pressable>
       ))}
      
