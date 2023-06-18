1eb393124767fbd9df791136cb1ae25257be503c
Koushik Mondal <darkdeavil004@gmail.com>
2023-06-18 12:30:16 +0530

    Work out Screen  Ui Done

--- screens/WorkoutScreen.js
+++ screens/WorkoutScreen.js
@@ -1,24 +1,45 @@
-import { StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView, Image, Pressable , ScrollView } from "react-native";
 import React from "react";
-import { useRoute } from "@react-navigation/native";
+import { useNavigation, useRoute } from "@react-navigation/native";
 import { Ionicons } from "@expo/vector-icons";
 
 const WorkoutScreen = () => {
   const route = useRoute();
+  const navigation = useNavigation();
   console.log(route.params);
   return (
-    <SafeAreaView style={styles.container}>
+    <>
+    <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
       <Image
         style={{ height: 170, width: "100%" }}
         source={{ uri: route.params.image }}
       />
       <Ionicons
+      onPress={()=> navigation.goBack()}
         name="arrow-back-outline"
-        size={24}
+        size={28}
         color="white"
-        style={{ position: "absolute", top: 50, left: 20 }}
+        style={{ position: "absolute", top: 20, left: 20 }}
       />
-    </SafeAreaView>
+
+      {route.params.excersises.map((item,index)=>(
+        <Pressable key={index} style={{margin:10,flexDirection:'row', alignItems:'center' }}>
+          <Image style={{height:90,width:90,}} source={{uri:item.image}} />
+          <View style={{marginLeft:10, }}>
+            <Text style={{fontSize:17, fontWeight:'bold'}}>{item.name}</Text>
+            <Text style={{fontSize:18, marginTop:4, color:'grey'}}>X{item.sets}</Text>
+          </View>
+        </Pressable>
+      ))}
+
+
+
+
+    </ScrollView>
+    <Pressable style={{marginLeft:'auto', marginRight:'auto', marginVertical:20,backgroundColor:'blue', padding:10,borderRadius:6, }}>
+      <Text style={{fontWeight:'600', fontSize:15,color:'white', textAlign:'center'}}>START</Text>
+    </Pressable>
+    </>
   );
 };
 
